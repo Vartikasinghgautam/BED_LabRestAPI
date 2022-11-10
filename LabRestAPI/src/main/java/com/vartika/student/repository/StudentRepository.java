6b8ea8a1dc77a2120e864172a219f7a3ece87cbb
Vartikasinghgautam <107786347+Vartikasinghgautam@users.noreply.github.com>
2022-11-10 19:45:15 +0530

    Add files via upload

---
+++ LabRestAPI/src/main/java/com/vartika/student/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.vartika.student.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vartika.student.model.Student;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+	
+	
+}
